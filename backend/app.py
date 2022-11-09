d952984880929e32e414366721328d37d2d5a7da
Oleg <74597383+winterka@users.noreply.github.com>
2022-11-09 18:51:23 +0300

    .

--- backend/app.py
+++ backend/app.py
@@ -5,7 +5,7 @@ from flask import send_file
 from flask import jsonify
 from models import get_tracks, post_track
 
-app = Flask(__name__, template_folder='../frontend/dist', static_folder='../fronten/dist/assets')
+app = Flask(__name__, template_folder='../frontend/dist', static_folder='../frontend/dist/assets')
 
 @app.route("/", methods = ["GET","POST"])
 def home():
@@ -16,8 +16,11 @@ def home():
         title = request.form.get('title')
         cover = request.form.get('cover')
         post_track(title, cover)
-        tracks = get_tracks()
-        return render_template('index.html', tracks = tracks)
+        return render_template('index.html', tracks = get_tracks())
+    return render_template('index.html', tracks = get_tracks())
+
+@app.route('/api/get-tracks',methods = ['GET'])
+def get_tracks_json():
     return jsonify(get_tracks())
 
 @app.route('/<audio_file_name>')
