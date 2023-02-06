77bbe47236dfe88ca2f375bca4611efde96c4da0
Oleg <74597383+winterka@users.noreply.github.com>
2023-02-06 06:38:21 +0300

    temp fix

--- backend/app.py
+++ backend/app.py
@@ -35,7 +35,7 @@ def get_cover(path):
            directory= 'src/audio',
            path = path
         )
-    return 1
+    return 'ok'
 
 if __name__ == '__main__':
     app.run(debug=True, host = "0.0.0.0")
